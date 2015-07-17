6b73f109d5781e1b15d585cdf717c9587999f1c6
Euan Mutch <euan.mutch@gmail.com>
2015-07-18 00:33:35 +0100

    Inital files

---
+++ desktop/src/com/ionsystems/monkeyfish/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ionsystems.monkeyfish.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ionsystems.monkeyfish.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
