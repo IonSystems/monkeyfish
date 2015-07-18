ce147ce123eec640480027519592f5b302298d3a
Cameron Craig <camieac@gmail.com>
2015-07-18 12:43:23 +0100

    Fix for android

--- android/src/com/ionsystems/monkeyfish/android/AndroidLauncher.java
+++ android/src/com/ionsystems/monkeyfish/android/AndroidLauncher.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.ionsystems.monkeyfish.Game;
+import com.ionsystems.monkeyfish.MonkeyFishGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Game(), config);
+		initialize(new MonkeyFishGame(), config);
 	}
 }
